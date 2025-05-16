63a9228946766e783173f6d1c9be920897e670d2
suffle8422 <inf.sky8422@gmail.com>
2025-05-16 13:54:51 +0900

    Initial Commit

---
+++ ArchitectureSample3/ArchitectureSample3App.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureSample3App.swift
+//  ArchitectureSample3
+//
+//  Created by Ibuki Onishi on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureSample3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchitectureSample3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArchitectureSample3
+//
+//  Created by Ibuki Onishi on 2025/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
